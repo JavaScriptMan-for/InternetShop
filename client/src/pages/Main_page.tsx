8960e96d98d5fe793fa6b29860e57cdec6545ec8
Pumastd <minevskiia10@gmail.com>
2025-03-24 16:15:03 +0700

    ddd

--- client/src/pages/Main_page.tsx
+++ client/src/pages/Main_page.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useWindow } from '../../src/hooks/useWindow.hook'
+import { Helmet } from 'react-helmet-async'
+
 
 const Main_page:FC = () => {
   const navigate = useNavigate();
   const windowSize = useWindow()
   return (
     <>
+    <Helmet>
+      <meta name="description" content="Добро пожаловать на Accessfull! Accessfull - интернет-магазин. Здесь Вы можете найти технику и аксессуары к ней" />
+    </Helmet>
     <main>
        <h1>Добро пожаловать на Accessfull</h1>
        <h2>Accessfull - интернет-магазин {windowSize.width < 700 && <br />} техники и аксессуаров к ней</h2>
