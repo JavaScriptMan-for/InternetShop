34f7689d66b1bcade3dbf43869ad71c96746aab9
Pumastd <minevskiia10@gmail.com>
2025-03-24 16:23:06 +0700

    micro

--- client/src/components/OpenGraph.tsx
+++ client/src/components/OpenGraph.tsx
@@ -14,7 +14,6 @@ const OpenGraph:FC<Props> = ({ product }) => {
 
   return (
     <Helmet>
-      <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
